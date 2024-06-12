7a7aa7479b5dd0d6fbb2b6944bcf0d9ea0bde5a4
Charles C <110767536+CSselrahC@users.noreply.github.com>
2024-06-11 18:35:38 -0800

    Add files via upload

---
+++ version 0.3/jmpTry.java
@@ -0,0 +1,544 @@
+package packageNaMalupet;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class jmpTry extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	int trackNumber, rowOfSelectedSong;
+	String songTitle,  trackNumberString, fileName;
+	JScrollPane scrollPane;
+	
+	JFileChooser fileChooser;
+	File musicFile0, musicFile1, musicFile2, musicFile3, musicFile4;
+	File musicFile5, musicFile6, musicFile7, musicFile8, musicFile9;
+	AudioInputStream inputAS;
+	Clip clip;
+	Clip clip0, clip1, clip2, clip3, clip4;
+	Clip clip5, clip6, clip7, clip8, clip9;
+	
+	JButton playButton, addButton, upButton, downButton, stopButton;
+	JButton loopAllButton, loopOneButton, shuffleButton, volumeUpButton, volumeDownButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jmpTry frame = new jmpTry();
+					frame.setResizable(false);
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public jmpTry() {
+		setTitle("JMusicPlayer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 420, 445);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Table
+		String[] columnNames = {"Track Number", "Song Title"};
+		String[][] data = {{"01", null},
+							{"02", null},
+							{"03", null},
+							{"04", null},
+							{"05", null},
+							{"06", null},
+							{"07", null},
+							{"08", null},
+							{"09", null},
+							{"10", null}};
+		
+		table = new JTable(data, columnNames);
+		
+		// Scroll Pane
+		scrollPane = new JScrollPane();
+		scrollPane.setViewportView(table);
+		scrollPane.setBounds(10, 22, 366, 137);
+		contentPane.add(scrollPane);
+		
+		// Up and Down Buttons
+		rowOfSelectedSong = 0;
+		table.setRowSelectionInterval(rowOfSelectedSong, rowOfSelectedSong);
+			
+		
+		// Up Button
+		upButton = new JButton("Up");
+		upButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (data[table.getSelectedRow()][1] != null) {
+					clip.stop();
+				}
+				
+				if (rowOfSelectedSong > 0) {
+					rowOfSelectedSong--;
+					table.setRowSelectionInterval(rowOfSelectedSong, rowOfSelectedSong);
+					System.out.println(rowOfSelectedSong);
+					
+					if (data[table.getSelectedRow()][1] != null) {
+						playButton.setEnabled(true);
+						playButton.setForeground(new Color(128, 255, 255));
+						playButton.setBackground(new Color(0, 0, 255));
+						if (table.getSelectedRow() == 0) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile0);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 1) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile1);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 2) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile2);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 3) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile3);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 4) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile4);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 5) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile5);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 6) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile6);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 7) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile7);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 8) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile8);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 9) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile9);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						}
+					} else {
+						playButton.setEnabled(false);
+						playButton.setBackground(new Color(127, 127, 127));
+					}
+					
+				}
+			}
+		});
+		upButton.setBounds(153, 221, 89, 23);
+		contentPane.add(upButton);
+		
+		// Down Button
+		downButton = new JButton("Down");
+		downButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (data[table.getSelectedRow()][1] != null) {
+					clip.stop();
+				}
+				
+				if (rowOfSelectedSong < 9) {
+					rowOfSelectedSong++;
+					table.setRowSelectionInterval(rowOfSelectedSong, rowOfSelectedSong);
+					System.out.println(rowOfSelectedSong);
+					
+					if (data[table.getSelectedRow()][1] != null) {
+						playButton.setEnabled(true);
+						if (table.getSelectedRow() == 0) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile0);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 1) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile1);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 2) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile2);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 3) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile3);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 4) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile4);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 5) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile5);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 6) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile6);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 7) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile7);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 8) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile8);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						} else if (table.getSelectedRow() == 9) {
+							try {
+								inputAS = AudioSystem.getAudioInputStream(musicFile9);
+								clip = AudioSystem.getClip();
+								clip.open(inputAS);
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							}
+						}
+					} else {
+						playButton.setEnabled(false);
+						playButton.setBackground(new Color(127, 127, 127));
+					}
+				}
+			}
+		});
+		downButton.setBounds(153, 311, 89, 23);
+		contentPane.add(downButton);
+		
+		// Play Music Button
+		playButton = new JButton("Play");
+		playButton.setEnabled(false);
+		playButton.setForeground(new Color(128, 255, 255));
+		playButton.setBackground(new Color(127, 127, 127));
+		playButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (playButton.getText() == "Play") {
+					clip.start();
+					playButton.setText("Pause");
+					playButton.setForeground(new Color(0, 0, 0));
+					playButton.setBackground(new Color(0, 255, 0));
+				} else {
+					clip.stop();
+					playButton.setText("Play");
+					playButton.setForeground(new Color(128, 255, 255));
+					playButton.setBackground(new Color(0, 0, 255));
+				}
+			}
+		});
+		playButton.setBounds(153, 265, 89, 23);
+		contentPane.add(playButton);
+		
+		// Stop Music Button
+		stopButton = new JButton("Stop");
+		stopButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		stopButton.setBounds(270, 265, 89, 23);
+		contentPane.add(stopButton);
+		
+		// Add Music Button
+		addButton = new JButton("Add");
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (data[table.getSelectedRow()][1] == null) {
+					fileChooser = new JFileChooser();
+					fileChooser.showOpenDialog(null);
+					
+					if (table.getSelectedRow() == 0) {
+						musicFile0 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile0);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile0.getName();
+						
+					} else if (table.getSelectedRow() == 1) {
+						musicFile1 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile1);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile1);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile1.getName();
+						
+					} else if (table.getSelectedRow() == 2) {
+						musicFile2 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile2);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile2.getName();
+						
+					} else if (table.getSelectedRow() == 3) {
+						musicFile3 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile3);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile3.getName();
+						
+					} else if (table.getSelectedRow() == 4) {
+						musicFile4 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile4);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile4.getName();
+						
+					} else if (table.getSelectedRow() == 5) {
+						musicFile5 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile5);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile5.getName();
+						
+					} else if (table.getSelectedRow() == 6) {
+						musicFile6 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile6);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile6);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile6.getName();
+						
+					} else if (table.getSelectedRow() == 7) {
+						musicFile7 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile7);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile7.getName();
+						
+					} else if (table.getSelectedRow() == 8) {
+						musicFile8 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile8);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile8.getName();
+						
+					} else if (table.getSelectedRow() == 9) {
+						musicFile9 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						System.out.println(musicFile0);
+						try {
+							inputAS = AudioSystem.getAudioInputStream(musicFile9);
+							clip = AudioSystem.getClip();
+							clip.open(inputAS);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						fileName = musicFile9.getName();
+					}
+					
+					data[table.getSelectedRow()][1] = fileName;
+					table = new JTable(data, columnNames);
+				}
+				
+				
+				if (data[0][1] != null || data[1][1] != null || data[2][1] != null || data[3][1] != null || data[4][1] != null || 
+						data[5][1] != null || data[6][1] != null || data[7][1] != null || data[8][1] != null || data[9][1] != null) {
+						playButton.setEnabled(true);
+						playButton.setForeground(new Color(128, 255, 255));
+						playButton.setBackground(new Color(0, 0, 255));
+				}
+				table.setRowSelectionInterval(rowOfSelectedSong, rowOfSelectedSong);
+			}
+		});
+		addButton.setBounds(10, 221, 89, 23);
+		contentPane.add(addButton);
+		
+		// Volume Up Button
+		volumeUpButton = new JButton("Volume Up");
+		volumeUpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		volumeUpButton.setBounds(10, 299, 89, 23);
+		contentPane.add(volumeUpButton);
+		
+		// Volume Down Button
+		volumeDownButton = new JButton("Volume Down");
+		volumeDownButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		volumeDownButton.setBounds(10, 326, 89, 23);
+		contentPane.add(volumeDownButton);
+		
+		// Loop All Music Button
+		loopAllButton = new JButton("Loop All");
+		loopAllButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		loopAllButton.setBounds(270, 345, 89, 23);
+		contentPane.add(loopAllButton);
+		
+		// Loop One Music Button
+		loopOneButton = new JButton("Loop");
+		loopOneButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		loopOneButton.setBounds(270, 311, 89, 23);
+		contentPane.add(loopOneButton);
+		
+		// Shuffle Button
+		shuffleButton = new JButton("Shuffle");
+		shuffleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		shuffleButton.setBounds(270, 372, 89, 23);
+		contentPane.add(shuffleButton);
+		
+		
+		stopButton.setEnabled(false);
+		loopAllButton.setEnabled(false);
+		loopOneButton.setEnabled(false);
+		shuffleButton.setEnabled(false);
+		volumeUpButton.setEnabled(false);
+		volumeDownButton.setEnabled(false);
+	}
+}
